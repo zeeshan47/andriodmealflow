461d77dbdf38531bb6cb9ec8dbbff13c27fac954
zeeshan47 <zeeshan47@users.noreply.github.com>
2026-06-16 14:49:46 +0500

    Release v37.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId  = "com.fastpos.android"
         minSdk = 24
         targetSdk      = 35
-        versionCode    = 5
-        versionName    = "36.1.1"
+        versionCode    = 6
+        versionName    = "37.0.0"
 
         buildConfigField("String", "GITHUB_UPDATE_OWNER", "\"zeeshan47\"")
         buildConfigField("String", "GITHUB_UPDATE_REPO", "\"andriodmealflow\"")
